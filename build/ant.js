3da45558ea6537e1619245bcf3f16890655a4732
antnativo <acrawford@nativo.net>
2017-07-06 17:55:49 -0700

    added class manipulation and toggle

--- build/ant.js
+++ build/ant.js
@@ -474,6 +474,30 @@ __.prototype.css = function (rule, value) {
   })
   return this;
 }
+__.prototype.addClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.add( className );
+  })
+  return this;
+}
+__.prototype.removeClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.remove( className );
+  })
+  return this;
+}
+__.prototype.toggleClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.remove( className );
+  })
+  return this;
+}
+__.prototype.toggle = function (className) { 
+  this.forEach(function (node) { 
+    node.style.visibility = (node.style.visibility != "visible") ? "visible" : hidden;
+  })
+  return this;
+}
     //Return new  __()
 return function (nodes, context) {
     var doc = (context) ? context : document;
--- src/style.js
+++ src/style.js
@@ -5,3 +5,27 @@ __.prototype.css = function (rule, value) {
   })
   return this;
 }
+__.prototype.addClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.add( className );
+  })
+  return this;
+}
+__.prototype.removeClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.remove( className );
+  })
+  return this;
+}
+__.prototype.toggleClass = function (className) { 
+  this.forEach(function (node) { 
+    node.classList.remove( className );
+  })
+  return this;
+}
+__.prototype.toggle = function (className) { 
+  this.forEach(function (node) { 
+    node.style.visibility = (node.style.visibility != "visible") ? "visible" : hidden;
+  })
+  return this;
+}
