162f5260ef8fd4c54a1b7b175949bdbfb7cf11b9
antnativo <acrawford@nativo.net>
2018-01-02 11:11:47 -0800

    adding animate function

--- src/animation.js
+++ src/animation.js
@@ -1,5 +1,22 @@
     //Animation
-    __.prototype.animate = function (cssValues,timeout, callback) {
-      callback()
+    __.prototype.animate = function (cssValues, timeout, callback) {
+      let duration = 200 / 1000,
+       sectionalTimeout = timeout / duration
+      const that = this
+      const anime = (function R(callback) {
+        let that =  this,
+         repeat = true
+        that.forEach(function (node) {
+          for (var key in cssValues)
+            //node.style[key] += (cssValues[key] / duration)
+            node.style[key] = cssValues[key]
+            repeat = node.style[key] != cssValues[key] 
+        })
+        if(repeat)
+          setTimeout(R.bind(that,callback), 0)
+        else
+          callback()
+      })
+      anime.call(this,callback)
       return this;
     };
